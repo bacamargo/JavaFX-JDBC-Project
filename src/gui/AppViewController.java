46ef42de162b9dbd033d20dd796ae4efb943d73c
bacamargo <bpcamargo9@hotmail.com>
2022-08-09 11:19:59 -0300

    SellerList

--- src/gui/AppViewController.java
+++ src/gui/AppViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
 public class AppViewController implements Initializable {
 
@@ -31,7 +32,10 @@ public class AppViewController implements Initializable {
 
     @FXML
     public void onMenuItemSellerAction() {
-        System.out.println("onMenuItemSellerAction");
+        loadView("/gui/SellerList.fxml", (SellerListController controller) -> {
+            controller.setSellerService(new SellerService());
+            controller.updateTableView();
+        });
     }
 
     @FXML
